8e78d70d959a9f755f958e0e75861b54bf858747
notinia <inaki.alfonso22@gmail.com>
2024-10-18 15:29:29 -0300

    avanceHTML

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { TopBarComponent } from "./top-bar/top-bar.component";
 import { HomeComponent } from "./home/home.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { MatIconModule } from '@angular/material/icon'
+import { CarrerasComponent } from "./carreras/carreras.component";
+import { LayoutComponent } from "./layout/layout.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, TopBarComponent, HomeComponent],
+  imports: [CommonModule, RouterOutlet, TopBarComponent, HomeComponent, NavbarComponent, MatIconModule, CarrerasComponent, LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
+
 export class HomeComponent {
 
 }
